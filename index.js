529ec56e323612cde2ff3eafe67567ee5186644e
Areej-fahad <102969380+Areej-fahad@users.noreply.github.com>
2022-11-17 08:24:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const fastify_1 = __importDefault(require("fastify"));
-const server = (0, fastify_1.default)({
-    logger: true,
-});
-server.route({
-    method: 'GET',
-    url: '/',
-    handler: (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
-        return { hello: "world" };
-    })
-});
+import Fastify from 'fastify'
+import { request } from 'http'
+const server = Fastify({
+logger:true,
+
+}
+)
+
+const users=["Ahmad", "Ali", "fahad"]
+const profil={
+name :"Ali",
+age : 27,
+location: "riyadh" ,
+}
+const posts={
+name : "nature",
+location: "Aurop"
+}
+server.get ("/",async(request,replay)=>{
+
+return("/users")
+  
+})
+server.get ("/",async(request,replay)=>{
+
+  return("/profil/Ali")
+    
+  })
+  server.get ("/",async(request,replay)=>{
+
+    return("/posts")
+      
+  })
+
+  
+
+
 server.listen({ port: 3000 }, (err, address) => {
-    if (err)
-        throw err;
-});
+    if (err) throw err
+
+  })
